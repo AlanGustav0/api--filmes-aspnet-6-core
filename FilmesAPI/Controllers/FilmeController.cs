69496bfcb4929ccb53a5e971d776f2458265dc30
AlanGustav0 <alagustavo946@gmail.com>
2023-01-13 16:46:31 -0300

    feat: start project

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,39 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody]Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(RecuperaFilmePorId),new {id = filme.Id},filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            return filme != null ? Ok(filme) : NotFound();
+
+        }
+    }
+}
