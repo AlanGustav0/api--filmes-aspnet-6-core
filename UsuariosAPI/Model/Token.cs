99e2101c8cec5a3b6bf6caea007f480044f1774f
AlanGustav0 <alagustavo946@gmail.com>
2024-01-19 11:53:39 -0300

    feature: Implements TokenService

---
+++ UsuariosAPI/Model/Token.cs
@@ -0,0 +1,11 @@
+﻿namespace UsuariosAPI.Model
+{
+    public class Token
+    {
+        public string Valor { get; }
+        public Token(string valor)
+        {
+            Valor = valor;
+        }
+    }
+}
